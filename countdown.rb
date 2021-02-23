bc5780bad1babbc7cce7dc54ecd61d9dfb595d77
Kyle Samuel <Ksamuel92@gmail.com>
2021-02-23 17:51:17 -0500

    commit tree 882e3b8e07992042e00e9fce87852e239ae37710

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,20 @@
 #write your code here
 
-def countdown
+def countdown(num)
+    count = num.to_i #Takes in arguement and sets where countdown begins
+    while count > 0
+        puts "#{count} SECOND(S)!"
+    count -= 1
+    end
+    "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(num)
+    count = num.to_i #Takes in arguement and sets where countdown begins
+    while count > 0
+        puts "#{count} SECOND(S)!"
+    count -= 1
+    sleep(1)
+    end
+    "HAPPY NEW YEAR!"
 end
